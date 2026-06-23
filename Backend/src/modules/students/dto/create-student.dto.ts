8c7aabe5057086194556f75b41fa5b7493ec31cd
Rafael Solis Ramos <rafaelsolioramoso@gmail.com>
2026-06-23 11:00:58 +0200

    implemente dto student et decorator

--- Backend/src/modules/students/dto/create-student.dto.ts
+++ Backend/src/modules/students/dto/create-student.dto.ts
@@ -1,6 +1,22 @@
+import {IsDateString, IsNotEmpty, IsOptional, IsString, MaxLength, MinLength} from "class-validator";
+
 export class CreateStudentDto {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(2)
+    @MaxLength(255)
     firstName: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(2)
+    @MaxLength(255)
     lastName: string;
+
+    @IsDateString()
     birthDate: string;
+
+    @IsOptional()
+    @IsString()
     photo?: string;
 }
