bb13574bd4c82d2d6f00832639b5e9a518eb3ffd
Rafael Solis Ramos <rafaelsolioramoso@gmail.com>
2026-06-30 14:30:45 +0200

    token-interceptor

---
+++ Frontend/src/app/core/interceptors/token.interceptor.ts
@@ -0,0 +1,18 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { AuthService } from '@core/services/auth.service';
+
+export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
+  const auth = inject(AuthService);
+  const token = auth.token();
+
+  if (!token) {
+    return next(req);
+  }
+
+  const authReq = req.clone({
+    setHeaders: { Authorization: `Bearer ${token}` },
+  });
+
+  return next(authReq);
+};
