5ad2f0a3293d4b8ac154d3f96e56726692691f25
Rafael Solis Ramos <rafaelsolioramoso@gmail.com>
2026-07-20 10:58:40 +0100

    fullscreen adaptation

--- Frontend/src/app/core/models/report-card.interface.ts
+++ Frontend/src/app/core/models/report-card.interface.ts
@@ -2,7 +2,7 @@ export type ReportCardStatus = 'DRAFT' | 'VALIDATED' | 'PUBLISHED' | 'SIGNED';
 
 export interface ReportCard {
   id: number;
-  status: ReportCardStatus;
+  status: ReportCardStatus ;
   student: { id: number };
   period: { id: number };
 }
